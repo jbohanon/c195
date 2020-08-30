8b39dd30d01c758fa852b02ebb1f71cdff0994f5
jbohanon <jacobbohanon@gmail.com>
2020-08-30 15:45:22 -0400

    appointment and user model done; working on appointment DAO

--- src/application/dao/AppointmentDAO.java
+++ src/application/dao/AppointmentDAO.java
@@ -2,8 +2,12 @@ package application.dao;
 
 import application.datamodel.Appointment;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
+
+import static application.dao.Database.dbUpdate;
+import static application.ui.ApplicationController.loggedInUser;
 
 public class AppointmentDAO implements DAO<Appointment> {
     @Override
@@ -13,7 +17,21 @@ public class AppointmentDAO implements DAO<Appointment> {
 
     @Override
     public boolean insert(Appointment appointment) {
-        return false;
+        String s = "INSERT INTO appointment (customerId, userId, " +
+                "title, description, location, contact, type, url, start, end, " +
+                "createDate, createdBy, lastUpdate, lastUpdateBy) VALUES (" +
+                appointment.getCustomer().getCustomerId() + ", " +
+                appointment.getUser().getUserId() + ", '" +
+                appointment.getTitle() + "', '" +
+                appointment.getDescription() + "', '" +
+                appointment.getLocation() + "', '" +
+                appointment.getContact() + "', '" +
+                appointment.getType().toString() + "', '" +
+                appointment.getUrl() + "', '" +
+                appointment.getStart().format(DateTimeFormatter.ISO_DATE_TIME) + "', '" +
+                appointment.getEnd().format(DateTimeFormatter.ISO_DATE_TIME) + "', " +
+                "now(), " + loggedInUser + ", now(), " + loggedInUser + ")";
+        return dbUpdate(s);
     }
 
     @Override
--- src/application/datamodel/User.java
+++ src/application/datamodel/User.java
@@ -1,4 +1,36 @@
 package application.datamodel;
 
 public class User {
+    private int _userId;
+    private String _userName;
+    private String _password;
+    private boolean _active;
+
+    public int getUserId() {
+        return _userId;
+    }
+    public void setUserId(int userId) {
+        _userId = userId;
+    }
+
+    public String getUserName() {
+        return _userName;
+    }
+    public void setUserName(String userName) {
+        _userName = userName;
+    }
+
+    public String getPassword() {
+        return _password;
+    }
+    public void setPassword(String password) {
+        _password = password;
+    }
+
+    public boolean getActive() {
+        return _active;
+    }
+    public void setActive(boolean active) {
+        _active = active;
+    }
 }
